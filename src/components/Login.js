c0e3bdb3add9175a70fc222c08aac602fd13ba31
Subhojit Paul <subhojitpaul21@gmail.com>
2020-03-20 09:07:03 +0530

    get user name on form submit

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,24 @@
 import React, {Component} from "react";
 
-class Login extends Component{
+class Login extends Component {
+  constructor(props) {
+    super(props);
+
+    this.submitHandler = this.submitHandler.bind(this);
+  }
+
+  submitHandler(event) {
+    console.log(event.target.name.value);
+    event.preventDefault();
+  }
+
   render() {
     return (
       <div>
         <h3>Please enter your name</h3>
-        <form>
+        <form onSubmit={this.submitHandler}>
           <div className="form-group">
-            <input type="text" className="form-control" id="name" />
+            <input type="text" className="form-control" id="name" required={true} />
           </div>
           <div className="btn-toolbar">
             <button type="submit" className="btn btn-success">Proceed</button>
