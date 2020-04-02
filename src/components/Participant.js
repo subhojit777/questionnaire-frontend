832990570390cb6c0d3897eb63bffdf8d2ee8e46
Subhojit Paul <subhojitpaul21@gmail.com>
2020-04-02 09:10:36 +0530

    remove usage of GitHub access token

--- src/components/Participant.js
+++ src/components/Participant.js
@@ -1,48 +1,29 @@
 import React, {Component} from 'react';
-import {parse} from "query-string";
 import {withCookies} from "react-cookie";
 import Question from "../components/Question";
 import Login from "./Login";
+import {string} from "prop-types";
 
 class Participant extends Component {
+  static propTypes = {
+    authenticationToken: string,
+  };
+
   constructor(props) {
     super(props);
 
+    this.authenticationToken = props.token;
     this.state = {
-      token: props.token,
       questions: props.questions,
       questionIndex: props.questionIndex,
     };
   }
 
-  componentWillMount() {
-    if (parse(this.props.location.search).code && !this.state.token) {
-      this.setGitHubAccessToken(parse(this.props.location.search).code);
-    }
-  }
-
-  setGitHubAccessToken(code) {
-    let getTokenUrl = new URL(`${process.env.REACT_APP_BACK_END_BASE_URL}/gh-access-token`);
-    getTokenUrl.search = new URLSearchParams({code: code}).toString();
-    const {cookies} = this.props;
-
-    fetch(getTokenUrl.toString())
-    .then(response => response.json())
-    .then(data => {
-      cookies.set('token', data);
-
-      this.setState({
-        token: data,
-      });
-    })
-    .catch(error => console.error(error));
-  }
-
   render() {
-    if (this.state.token) {
+    if (this.authenticationToken) {
       let question = this.state.questions[this.state.questionIndex];
       return (
-        <Question question={question} token={this.state.token} />
+        <Question question={question} token={this.authenticationToken} />
       );
     }
     else {
